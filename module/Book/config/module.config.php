06f34b4493df296245305e0246a23d86137f97a7
Romain Hardy <romain.hardy17@gmail.com>
2013-03-25 21:34:19 +0100

    initialize Book Controller and views

--- module/Book/config/module.config.php
+++ module/Book/config/module.config.php
@@ -5,9 +5,29 @@ return array(
             'Book\Controller\Book' => 'Book\Controller\BookController',
         ),
     ),
+
+    // Routing:
+    'router' => array(
+        'routes' => array(
+            'book' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/book[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Book\Controller\Book',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),    
     'view_manager' => array(
         'template_path_stack' => array(
-            'album' => __DIR__ . '/../view',
+            'book' => __DIR__ . '/../view',
         ),
     ),
 );
---
+++ module/Book/src/Book/Controller/BookController.php
@@ -0,0 +1,20 @@
+<?php
+namespace Book\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class BookController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
